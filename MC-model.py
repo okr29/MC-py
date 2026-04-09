84c8a4d6fa65c5cb09d23f24f1e537bbd08400bb
KrylO <ondrej.kryl@natur.cuni.cz>
2026-04-09 16:07:03 +0200

    epsilon a and v

--- MC-model.py
+++ MC-model.py
@@ -12,7 +12,7 @@ print(data.head())
 
 # print(data.columns.tolist())
 
-data.plot(x='epsilon_a', y='q', kind='scatter')
+# data.plot(x='epsilon_a', y='q', kind='scatter')
 # plt.show()
 
 qmax = data['q'].max()
@@ -31,13 +31,51 @@ my_custom_cap = 0.05
 x_function = np.linspace(0, my_custom_cap)
 y_function = E_50 * x_function
 
-plt.plot(data['epsilon_a'], data['q'], label='Original Data')
-plt.plot(x_function, y_function, label='E50 Secant Line', linestyle='-', color='red')
-plt.plot(epsilon_a_q_50, q_50, 'x', markersize=20)
+
+
+
+processed_data = data.assign(epsilon_v=lambda x: x['epsilon_a'] - x['epsilon_r'] * 2)
+print(processed_data.head())
+
+epsilon_v_min = processed_data['epsilon_v'].min()
+print(f'Minimum epsilon_v value: {epsilon_v_min}')
+
+# 1. Find the index (row number) 
+closest_index2 = (processed_data['epsilon_v'] - epsilon_v_min).abs().idxmin()
+# 2. Grab the value at that specific row
+epsilon_a_epsilon_v_min = data.loc[closest_index2, 'epsilon_a']
+print(f'Closest epsilon_a value for epsilon_v_min: {epsilon_a_epsilon_v_min}')
+
+
+# nu = processed_data['epsilon_r'] / processed_data['epsilon_a']
+# print(nu.head(50))
+
+
+################################ PLOTOVANI ##########################################
+################################ epsilon_a, q ##########################################
+# plt.scatter(data['epsilon_a'], data['q'], label='Original Data')
+# plt.plot(x_function, y_function, label='E50 Secant Line', linestyle='-', color='red')
+# plt.plot(epsilon_a_q_50, q_50, 'x', markersize=20)
+# plt.xlabel('epsilon_a')
+# plt.ylabel('q')
+# plt.legend()
+# plt.show()
+
+################################ epsilon_a, epsilon_v ##########################################
+plt.scatter(processed_data['epsilon_a'], processed_data['epsilon_v'], label='Original Data')
+plt.plot(np.linspace(0, processed_data['epsilon_a'].max()), np.linspace(0, 0), label='0 Line', linestyle='--', color='red')
 plt.xlabel('epsilon_a')
-plt.ylabel('q')
+plt.ylabel('epsilon_v')
 plt.legend()
 plt.show()
+
+# ################################ p, q ##########################################
+# plt.scatter(processed_data['p'], processed_data['q'], label='Original Data')
+# plt.xlabel('p (kPa)')
+# plt.ylabel('q (kPa)')
+# plt.legend()
+# plt.show()
+
 
 
 # E_50 = q_50 / 
