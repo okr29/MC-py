3f39e594b8ea929b0b3243f18de733e389eca426
KrylO <ondrej.kryl@natur.cuni.cz>
2026-04-09 14:46:37 +0200

    plot q, epsilon

--- MC-model.py
+++ MC-model.py
@@ -1,8 +1,14 @@
 import pandas
+import matplotlib.pyplot as plt
 
 def load_data(file_path):
-    data = pandas.read_csv(file_path)
+    data = pandas.read_csv(file_path, sep=r'\s+')
     return data
 
 data = load_data('115.dat')
 print(data.head())
+
+# print(data.columns.tolist())
+
+data.plot(x='epsilon_a', y='q', kind='scatter')
+plt.show()
